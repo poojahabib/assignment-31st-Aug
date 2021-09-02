26a8bf70790d667c58e5073ec94e1783f4f9ab7d
poojahabib <89843027+poojahabib@users.noreply.github.com>
2021-09-02 20:48:30 +0530

    Add files via upload

---
+++ ArrayDescendingOrder.java
@@ -0,0 +1,33 @@
+package com.yash.array;
+
+public class ArrayDescendingOrder {
+
+	public static void main(String[] args) {
+		int [] arr = new int [] {8,2,3,1,6,5,7};   
+        int temp = 0;  
+            
+        for (int i = 0; i < arr.length; i++) {   
+            for (int j = i+1; j < arr.length; j++) {   
+               if(arr[i] < arr[j]) {  
+                   temp = arr[i];  
+                   arr[i] = arr[j];  
+                   arr[j] = temp;  
+               }   
+            }   
+        }  
+          
+        System.out.println();  
+          
+        System.out.println("Array sorted in descending order: ");  
+        for (int i = 0; i < arr.length; i++) {   
+            System.out.print(arr[i] + " ");  
+        }  
+    }  
+}  
+		
+		
+		
+
+	
+
+
