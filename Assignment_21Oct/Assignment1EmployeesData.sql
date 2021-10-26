47834520a8df952df89a209e559482683c7c4ba4
poojahabib <89843027+poojahabib@users.noreply.github.com>
2021-10-26 18:42:38 +0530

    Add files via upload

---
+++ Assignment_21Oct/Assignment1EmployeesData.sql
@@ -0,0 +1,10 @@
+select email, phone_number from employees where department_id ='10';
+select first_name, last_name, salary, commission_pct from employees where salary>=10000 and commission_pct>0.2;
+select first_name, last_name, salary, department_id from employees where department_id in (10,20,50) order by department_id asc;
+select first_name from employees  where first_name like 'K%' or '%n';
+select last_name from employees where last_name like '%oc%';
+select first_name, last_name, commission_pct from employees where commission_pct is null;
+select distinct(manager_id) from employees;
+select * from employees order by salary desc;
+select * from employees where department_id='50' and salary>5000;
+select * from employees where (job_id='SA_REP' or 'SA_MAN');
